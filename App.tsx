8ec4d595b54d842f6d5f788160bff39d6334fa4c
rainyjune <rainyjune@live.cn>
2025-03-26 22:09:25 +0800

    Updated player screen

--- App.tsx
+++ App.tsx
@@ -5,12 +5,19 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
+import TrackPlayer from 'react-native-track-player';
 import RootStack from './src/navigation/AppNavigator';
 
 function App() {
+  const setupPlayer = async () => {
+    await TrackPlayer.setupPlayer();
+  };
+  useEffect(() => {
+    setupPlayer();
+  }, []);
   return (
     <NavigationContainer>
       <RootStack />
--- src/screens/PlayerScreen.tsx
+++ src/screens/PlayerScreen.tsx
@@ -1,16 +1,21 @@
-import {useNavigation} from '@react-navigation/native';
 import {Button, Text, View} from 'react-native';
+import TrackPlayer from 'react-native-track-player';
 
 export default function PlayerScreen() {
-  const navigation = useNavigation();
+  const handlePlay = async () => {
+    await TrackPlayer.add({
+      id: 'track1',
+      url: 'https://ra-sycdn.kuwo.cn/b07d1a4e2f9253bcae57edc2efef24ba/67e40300/resource/n1/2011/06/25/3348130080.mp3', // 示例URL
+      title: 'Sample Audio',
+      artist: 'SoundHelix',
+    });
+    await TrackPlayer.play();
+  };
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Player Screen</Text>
-      <Button
-        title="Back to Home"
-        onPress={() => navigation.navigate('Home')}
-      />
+      <Button title="Play" onPress={() => handlePlay()} />
     </View>
   );
 }
